be98dba6397097330e8358ec8bf002ebf2853aaa
gopalroy24 <gopalsdiary@gmail.com>
2026-05-08 23:49:02 +0600

    afas

--- src/lib/supabase/typed-client.ts
+++ src/lib/supabase/typed-client.ts
@@ -1,8 +1,3 @@
-import { createClient as createBaseClient } from "@supabase/supabase-js";
-import { Database } from "@/types/supabase";
+import { supabase } from "./client";
 
-export const createClient = () =>
-  createBaseClient<Database>(
-    import.meta.env.VITE_SUPABASE_URL!,
-    import.meta.env.VITE_SUPABASE_ANON_KEY!
-  );
+export const createClient = () => supabase;
